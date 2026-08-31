e0a800240e741e627a2dea8804f4876b3ea70f76
Gabriel Câmara <gabriel25camr@gmail.com>
2026-08-31 11:52:08 -0300

    Primeiro commit - Projeto Spring Boot inicial

---
+++ src/main/java/gabriel/crud_basico_java/CrudBasicoJavaApplication.java
@@ -0,0 +1,13 @@
+package gabriel.crud_basico_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasicoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasicoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabriel/crud_basico_java/CrudBasicoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.crud_basico_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasicoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
