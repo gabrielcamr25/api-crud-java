b23a6c8e79fc5e1e6ce28f3f17cb70c530366370
Gabriel Câmara <gabriel25camr@gmail.com>
2026-09-03 10:41:01 -0300

    feat: CRUD, DB connection

---
+++ src/main/java/gabriel/crud_basico_java/controller/JsonProcessingException.java
@@ -0,0 +1,4 @@
+package gabriel.crud_basico_java.controller;
+
+public class JsonProcessingException extends Exception {
+}
--- src/main/java/gabriel/crud_basico_java/controller/ListController.java
+++ src/main/java/gabriel/crud_basico_java/controller/ListController.java
@@ -1,2 +1,42 @@
-package gabriel.crud_basico_java.controller;public class ListController {
+package gabriel.crud_basico_java.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ListController {
+    private List<String> tsks = new ArrayList<>();
+    private ObjectMapper  objectMapper;
+    public ListController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+    @GetMapping(path="tasks/list")
+    public ResponseEntity<String> listTasks() throws JsonProcessingException{
+        System.out.println("Tarefas listadas com sucesso ");
+        return ResponseEntity.ok(objectMapper.writeValueAsString(tsks));
+    }
+    @PostMapping(path="tasks/create")
+    public ResponseEntity<Void> createTasks(@RequestBody String toDo){
+        tsks.add(toDo);
+        System.out.println("Tarefas adicionadas com sucesso");
+        return ResponseEntity.ok().build();
+
+    }
+    @DeleteMapping(path="tasks/delete")
+    public ResponseEntity<Void> clearTasks(){
+        tsks =  new ArrayList<>();
+        System.out.println("Tarefas removidas com sucesso");
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(path="tasks/edit")
+    public ResponseEntity<Void> editTasks(){
+        System.out.println("Tarefas editadas com sucesso");
+        return ResponseEntity.ok().build();
+    }
 }
